b7762177f5ed845cb2b608eb36859633f8912474
KaiqueVfreitas <kaiqueffreitasvieira@gmail.com>
2024-12-16 18:12:40 -0300

    Models pessoas.py estruturada

---
+++ Aplicação_de_POO_e_Arquitetura_MVC/src/models/pessoas.py
@@ -0,0 +1,8 @@
+class Pessoa:
+    def __init__(self, nome, sobrenome, idade, cpf):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__idade = idade
+        self.__cpf = cpf
+    def nome_completo(self):
+        return self.nome + self.sobrenome 
